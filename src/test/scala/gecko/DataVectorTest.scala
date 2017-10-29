7f7e424ca408b5e53e9f218493b91bda6d0a8c02
Robert Soeldner <r.soeldner@gmail.com>
2017-10-29 10:37:04 +0100

    Add DataVector test for map and flatmap

--- src/test/scala/gecko/DataVectorTest.scala
+++ src/test/scala/gecko/DataVectorTest.scala
@@ -1,11 +1,21 @@
 package gecko
 
 import org.scalatest.{FlatSpec, Matchers}
-
+import syntax._
 
 class DataVectorTest extends FlatSpec with Matchers {
 
   behavior of "DataVector"
+
+  it should "map correct" in {
+    val vec = DataVector.fromArray(Array[Int](1,2,3,4,5))
+    vec.map(_ + 10).underlying shouldBe(Array(11,12,13,14,15))
+  }
+
+  it should "flatmap correct" in {
+    val vec = DataVector.fromArray(Array[Int](1,2,3,4,5))
+    vec.flatMap(x => Array(x + 10).toDataVector).underlying shouldBe(Array(11,12,13,14,15))
+  }
 
   it should "shift up correct" in {
     val vec = DataVector.fromArray(Array[Int](1,2,3,4,5))
