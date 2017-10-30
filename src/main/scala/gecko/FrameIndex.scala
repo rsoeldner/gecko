41fa9c2100196d7be4c633b590bd8fba773eb7dd
Robert Soeldner <r.soeldner@gmail.com>
2017-10-30 08:04:06 +0100

    Add combine FameIndex

--- src/main/scala/gecko/FrameIndex.scala
+++ src/main/scala/gecko/FrameIndex.scala
@@ -1,7 +1,7 @@
 package gecko
 
 import scala.reflect.ClassTag
-import scala.collection.mutable.ListBuffer
+
 final case class FrameIndex[@specialized(Int, Double, Boolean, Long) A: ClassTag](
     underlying: Array[A],
     private[gecko] val indexes: Array[Int]
@@ -16,6 +16,13 @@ final case class FrameIndex[@specialized(Int, Double, Boolean, Long) A: ClassTag
     val underlyingSlice = copyRange(underlying, begin, end)
     val indexSlice = copyRange(indexes, begin, end)
     FrameIndex(underlyingSlice, indexSlice)
+  }
+
+  def ++(other: FrameIndex[A]) = {
+    val size = length + other.length
+    val ix = Array.range(0, size)
+    val values = Array.concat(underlying, other.underlying)
+    FrameIndex(values, ix)
   }
 
   /** Remove elements at index i
